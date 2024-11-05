f63ccf62b2bd602ef5f0f30184ba9f8e260cd710
Mikael Pimentel <mikaelajoysalazarpimentel@gmail.com>
2024-11-05 19:45:30 +0800

    Create PrintName.java

---
+++ PrintName.java
@@ -0,0 +1,10 @@
+import myPackage.Myclass;
+public class PrintName {
+  public static void main (String[] args)
+
+  String name= "GeeksforGeeks";
+  MyClass obj = new MyClass();
+
+ obj.getNames(name);
+}
+}
